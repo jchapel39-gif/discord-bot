e24ed8d09586ae5bca9366824caced163c577734
jchapel39-gif <j.chapel39@gmail.com>
2025-12-10 09:35:41 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,20 +1,20 @@
-import os
 import discord
 from discord.ext import commands
+import os
 
 intents = discord.Intents.default()
-intents.message_content = True
+intents.message_content = True  # Obligatoire pour lire les messages
 
-bot = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f'{bot.user} est connecté !')
+    print(f"{bot.user} est connecté !")
 
 @bot.command()
 async def ping(ctx):
-    await ctx.send('Pong !')
+    await ctx.send("Pong !")
 
-bot.run(os.getenv('DISCORD_TOKEN'))
+bot.run(os.getenv("DISCORD_TOKEN"))  # Utilise une variable d'environnement pour le token
 
 
